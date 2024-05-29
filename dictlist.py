abe3d6cbe5a9ae22429b4554d8fdb8dca18f5448
tamunoWoks <tamunosakiwokoma86@gmail.com>
2024-05-28 22:21:39 -0600

    Create and initialize list of dictionaries variable

--- dictlist.py
+++ dictlist.py
@@ -1,5 +1,10 @@
 def main():
-    ...
+    info = [
+    {'fname':'Tammy', 'lname':'Wokoma', 'hometown':'Buguma', 'status':'is Married'},
+    {'fname':'Esther', 'lname':'Dimkpa', 'hometown':'Ebubu', 'status':'is Married'},
+    {'fname':'Nene', 'lname':'Nnaji', 'hometown':'Nbawsi', 'status':'is Single'},
+    {'fname':'Ibiba', 'lname':'Longjohn', 'hometown':'Bonny', 'status':None}
+        ]
 
 if __name__ == '__main__':
     main()
