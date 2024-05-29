4bb5de95408e5e2b8f4e4956a12a58debcc8b109
tamunoWoks <tamunosakiwokoma86@gmail.com>
2024-05-28 22:25:43 -0600

    Cleaned up code

--- dictlist.py
+++ dictlist.py
@@ -1,13 +1,33 @@
 def main():
     info = [
-    {'fname':'Tammy', 'lname':'Wokoma', 'hometown':'Buguma', 'status':'is Married'},
-    {'fname':'Esther', 'lname':'Dimkpa', 'hometown':'Ebubu', 'status':'is Married'},
-    {'fname':'Nene', 'lname':'Nnaji', 'hometown':'Nbawsi', 'status':'is Single'},
-    {'fname':'Ibiba', 'lname':'Longjohn', 'hometown':'Bonny', 'status':None}
-        ]
+        {
+            "fname": "Tammy",
+            "lname": "Wokoma",
+            "hometown": "Buguma",
+            "status": "is Married",
+        },
+        {
+            "fname": "Esther",
+            "lname": "Dimkpa",
+            "hometown": "Ebubu",
+            "status": "is Married",
+        },
+        {
+            "fname": "Nene",
+            "lname": "Nnaji",
+            "hometown": "Nbawsi",
+            "status": "is Single",
+        },
+        {"fname": "Ibiba", "lname": "Longjohn", "hometown": "Bonny", "status": None},
+    ]
     for people in info:
-        people["status"] = people["status"] if people["status"] is not None else "status is unknown"
-        print(f"{people['fname']} {people['lname']} is from {people['hometown']} and {people['status']}")
+        people["status"] = (
+            people["status"] if people["status"] is not None else "status is unknown"
+        )
+        print(
+            f"{people['fname']} {people['lname']} is from {people['hometown']} and {people['status']}"
+        )
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
